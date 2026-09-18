a39a6ea3e1993831fbaf940b9814e9e3e53a7344
Yuvraj Singh <thinkyuvraj@gmail.com>
2026-09-19 03:54:56 +0530

    Reorder import statements in HomePage.tsx

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { Navbar } from './components/Navbar';
 import { MarqueeTicker } from './components/MarqueeTicker';
 import { Hero } from './components/Hero';
